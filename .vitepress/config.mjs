391baf84843b112c22547471e177479885336fe0
Daniel Santillan <daniel.santillan@eox.at>
2024-06-21 13:32:01 +0200

    initial configuration

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "eodash Pages Template",
-  description: "This is a template example of how to setup eodash within a VitePress environment",
+  title: "GTIF Cerulean",
+  description: "Project information and service endpoint",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'UC1 Dashboard', link: '/uc1dashboard/' },
-      { text: 'UC2 Dashboard', link: '/uc2dashboard/' },
+      { text: 'Dashboard', link: '/uc1dashboard/' },
+      // { text: 'UC2 Dashboard', link: '/uc2dashboard/' },
       { text: 'Stories', link: '/stories/' },
       { text: 'Editor', link: '/editor/' }
     ],

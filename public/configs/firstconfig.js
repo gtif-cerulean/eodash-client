558d484ed3c0c0955ae4cb2bd125b6522717c9dc
Lubomir Dolezal <lubomir.dolezal@eox.at>
2024-07-15 21:30:29 +0200

    adapt STACinfo according to trilateral from eodash v4

--- public/configs/firstconfig.js
+++ public/configs/firstconfig.js
@@ -61,13 +61,27 @@ export default {
                     for: store.states.currentUrl,
                     allowHtml: "true",
                     styleOverride:
-                      "#properties li > .value {font-weight: normal !important;}",
-                    header: "[]",
-
+                      `.single-property {columns: 1!important;}
+                      h1 {margin:0px!important;font-size:16px!important;}
+                      header h1:after {
+                        content:' ';
+                        display:block;
+                        border:1px solid #d0d0d0;
+                      }
+                      h2 {font-size:15px}
+                      h3 {font-size:14px}
+                      summary {cursor: pointer;}
+                      #properties li > .value { font-weight: normal !important;}
+                      main {padding-bottom: 10px;}
+                      .footer-container {line-height:1;}
+                      .footer-container button {margin-top: -10px;}
+                      .footer-container small {font-size:10px;line-height:1;}`,
+                    header: '["title"]',
+                    tags: '["themes"]',
                     subheader: "[]",
-                    properties: '["description"]',
-                    featured: "[]",
-                    footer: "[]",
+                    properties: '["satellite","sensor","agency","extent"]',
+                    featured: '["description","providers","assets","links"]',
+                    footer: '["sci:citation"]',
                   },
                   tagName: "eox-stacinfo",
                 },

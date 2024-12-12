450f92a794a9aa66f0309fbfb504d0b77f2fca00
A-Behairi <behairiahmed@gmail.com>
2024-12-12 16:31:25 +0100

    update packages and import stacinfo

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -16,6 +16,7 @@ export default {
       await import('../../.eodash/dist/eo-dash')
       await import('../../.eodash/dist/style.css')
       await import("@eox/storytelling")
+      await import("@eox/stacinfo")
     }
   }
 }
